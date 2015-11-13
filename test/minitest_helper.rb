2eb6004814ba5298852c7843d8ce6a14fa0a221d
Brad Schneider <bschneider@containerstore.com>
2015-11-13 09:34:19 -0600

    Added minitest reporters to make with the pretty colors.

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -2,3 +2,5 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'mark_maker'
 require 'active_support/core_ext/string'
 require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new
